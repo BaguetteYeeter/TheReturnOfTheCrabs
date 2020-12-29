5a09575088b7ef27dd2ecaabd14c04ca6fb1891c
BaguetteYeeter <baguetteyeeter@gmail.com>
2020-12-29 20:29:19 +0000

    Added crab rave beta

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 import getopt
+import random
+import math
 
 pack = "default"
 
@@ -23,11 +25,22 @@ pygame.display.set_caption("The Return Of The Crabs")
 
 minigameMenu = False
 menu = True
+rave = False
+arrowIsActive = 0
+score = 0
+hotel = "Trivago"
 
 class graphics:
     print("Loading textures")
     title = pygame.image.load("textures/" + pack + "/menu/menu/title.png")
     gameMenu = pygame.image.load("textures/" + pack + "/menu/minigamemenu/menu.png")
+    class rave:
+        back = pygame.image.load("textures/" + pack + "/rave/back.png")
+        crab = pygame.image.load("textures/" + pack + "/rave/crab.png")
+        upArrow = pygame.image.load("textures/" + pack + "/rave/up.png")
+        downArrow = pygame.image.load("textures/" + pack + "/rave/down.png")
+        leftArrow = pygame.image.load("textures/" + pack + "/rave/left.png")
+        rightArrow = pygame.image.load("textures/" + pack + "/rave/right.png")
     print("Done")
 
 run = True
@@ -49,13 +62,99 @@ while run:
     if minigameMenu:
         mainScreen.blit(graphics.gameMenu, (0, 0))
         if keys[pygame.K_r]:
-            print("game not done")
-            run = False
+            minigameMenu = False
+            rave = True
         if keys[pygame.K_l]:
             print("game not done")
             run = False
         if keys[pygame.K_b]:
             print("game not done")
             run = False
+    if rave:
+        mainScreen.blit(graphics.rave.back, (0, 0))
+
+        if arrowIsActive == 0:
+            arrowDirection = random.randint(1,4)
+            if arrowDirection == 1:
+                arrowDirection = "up"
+                arrowIsActive = 1
+            elif arrowDirection == 2:
+                arrowDirection = "down"
+                arrowIsActive = 1
+            elif arrowDirection == 3:
+                arrowDirection = "left"
+                arrowIsActive = 1
+            elif arrowDirection == 4:
+                arrowDirection = "right"
+                arrowIsActive = 1
+        if arrowDirection == "up":
+            mainScreen.blit(graphics.rave.upArrow, (50, 320))
+
+        elif arrowDirection == "down":
+            mainScreen.blit(graphics.rave.downArrow, (50, 420))
+
+        elif arrowDirection == "left":
+            mainScreen.blit(graphics.rave.leftArrow, (0, 370))
+
+        elif arrowDirection == "right":
+            mainScreen.blit(graphics.rave.rightArrow, (100, 370))
+
+        else:
+            mainScreen.blit(graphics.rave.crab, (400, 255))
+
+        if keys[pygame.K_UP]:
+            if arrowDirection == "up":
+                arrowIsActive = 0
+                score += 1
+                mainScreen.blit(graphics.rave.crab, (400, 185))
+            else:
+                arrowIsActive = 1
+                score -= 1
+                mainScreen.blit(graphics.rave.crab, (400, 185))
+        elif keys[pygame.K_DOWN]:
+            if arrowDirection == "down":
+                arrowIsActive = 0
+                score += 1
+                mainScreen.blit(graphics.rave.crab, (400, 325))
+            else:
+                arrowIsActive = 1
+                score -= 1
+                mainScreen.blit(graphics.rave.crab, (400, 325))
+        elif keys[pygame.K_LEFT]:
+            if arrowDirection == "left":
+                arrowIsActive = 0
+                score += 1
+                mainScreen.blit(graphics.rave.crab, (300, 255))
+            else:
+                arrowIsActive = 1
+                score -= 1
+                mainScreen.blit(graphics.rave.crab, (300, 255))
+        elif keys[pygame.K_RIGHT]:
+            if arrowDirection == "right":
+                arrowIsActive = 0
+                score += 1
+                mainScreen.blit(graphics.rave.crab, (500, 255))
+            else:
+                arrowIsActive = 1
+                score -= 1
+                mainScreen.blit(graphics.rave.crab, (500, 255))
+        else:
+            mainScreen.blit(graphics.rave.crab, (400, 255))
+
+        mainScreen.blit(pygame.image.load("textures/" + pack + "/rave/score/score.png"), (0, 0))
+
+        if score < 10 and score >= 0:
+            mainScreen.blit(pygame.image.load("textures/" + pack + "/rave/score/" + str(score)+ ".png"), (240, 0))
+        if score >= 10 and score < 100:
+            mainScreen.blit(pygame.image.load("textures/" + pack + "/rave/score/" + str(math.floor(score/10))+".png"), (240, 0))
+            mainScreen.blit(pygame.image.load("textures/" + pack + "/rave/score/" + str(score - math.floor(score/10)*10)+".png"), (288, 0))
+        if score >= 100:
+            print("Crab Rave Complete!")
+            run = False
+
+
+
+
+
 
     pygame.display.update()

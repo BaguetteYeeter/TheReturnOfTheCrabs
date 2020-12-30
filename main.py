65eae2feaa8457d2c49ab42d87583a3b9b046b63
BaguetteYeeter <baguetteyeeter@gmail.com>
2020-12-30 13:37:06 +0000

    added cutscenes

--- main.py
+++ main.py
@@ -3,23 +3,29 @@ import sys
 import getopt
 import random
 import math
+import time
 
 pack = "default"
+score = 0
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "hp:", ["pack="])
+    opts, args = getopt.getopt(sys.argv[1:], "hp:s:", ["pack=", "score="])
 except getopt.GetoptError:
-    print("main.py -p [texture pack]")
+    print("main.py -p [texture pack] -s [score]")
     sys.exit(2)
 for opt, arg in opts:
     if opt == "-h":
-        print("main.py -p [texture pack]")
+        print("main.py -p [texture pack] -s [score]")
         sys.exit()
     elif opt in ("-p", "--pack"):
         pack = arg
+    elif opt in ("-s", "--score"):
+        score = int(arg)
 
 pygame.init()
 mainScreen = pygame.display.set_mode((640, 480))
+pygame.font.init()
+scoreFont = pygame.font.SysFont('Comic Sans MS', 90)
 
 pygame.display.set_caption("The Return Of The Crabs")
 
@@ -27,8 +33,8 @@ minigameMenu = False
 menu = True
 rave = False
 arrowIsActive = 0
-score = 0
 hotel = "Trivago"
+cutscene = 0
 
 class graphics:
     print("Loading textures")
@@ -41,6 +47,12 @@ class graphics:
         downArrow = pygame.image.load("textures/" + pack + "/rave/down.png")
         leftArrow = pygame.image.load("textures/" + pack + "/rave/left.png")
         rightArrow = pygame.image.load("textures/" + pack + "/rave/right.png")
+        cutscene1 = pygame.image.load("textures/" + pack + "/rave/cutscenes/cut1.png")
+        cutscene2 = pygame.image.load("textures/" + pack + "/rave/cutscenes/cut2.png")
+        cutscene3 = pygame.image.load("textures/" + pack + "/rave/cutscenes/cut3.png")
+        cutscene4 = pygame.image.load("textures/" + pack + "/rave/cutscenes/cut4.png")
+        cutscene5 = pygame.image.load("textures/" + pack + "/rave/cutscenes/cut5.png")
+        cutscene6 = pygame.image.load("textures/" + pack + "/rave/cutscenes/cut6.png")
     print("Done")
 
 run = True
@@ -141,20 +153,56 @@ while run:
         else:
             mainScreen.blit(graphics.rave.crab, (400, 255))
 
-        mainScreen.blit(pygame.image.load("textures/" + pack + "/rave/score/score.png"), (0, 0))
+        finalFont = scoreFont.render("SCORE: " + str(score), False, (0, 0, 0))
+        mainScreen.blit(finalFont, (0, 0))
+        if cutscene == 2:
+            mainScreen.blit(graphics.rave.cutscene2, (0, 0))
 
-        if score < 10 and score >= 0:
-            mainScreen.blit(pygame.image.load("textures/" + pack + "/rave/score/" + str(score)+ ".png"), (240, 0))
-        if score >= 10 and score < 100:
-            mainScreen.blit(pygame.image.load("textures/" + pack + "/rave/score/" + str(math.floor(score/10))+".png"), (240, 0))
-            mainScreen.blit(pygame.image.load("textures/" + pack + "/rave/score/" + str(score - math.floor(score/10)*10)+".png"), (288, 0))
-        if score >= 100:
-            print("Crab Rave Complete!")
-            run = False
+            if keys[pygame.K_RETURN]:
+                pygame.time.delay(10)
+                cutscene = 3
+        elif cutscene == 3:
+            mainScreen.blit(graphics.rave.cutscene3, (0, 0))
 
+            if keys[pygame.K_RETURN]:
+                pygame.time.delay(10)
+                cutscene = 4
+        elif cutscene == 4:
+            mainScreen.blit(graphics.rave.cutscene4, (0, 0))
 
+            if keys[pygame.K_RETURN]:
+                pygame.time.delay(10)
+                cutscene = 5
+        elif cutscene == 5:
+            mainScreen.blit(graphics.rave.cutscene5, (0, 0))
 
+            if keys[pygame.K_RETURN]:
+                pygame.time.delay(10)
+                cutscene = 6
+        elif cutscene == 6:
+            mainScreen.blit(graphics.rave.cutscene6, (0, 0))
 
+            if keys[pygame.K_RETURN]:
+                pygame.time.delay(10)
+                rave = False
+                menu = True
+            if keys[pygame.K_r]:
+                score = 0
+                cutscene = 0
+        elif score == 100:
+            mainScreen.blit(graphics.rave.cutscene1, (0, 0))
 
+            cutscene = 1
+            if keys[pygame.K_RETURN]:
+                pygame.time.delay(10)
+                cutscene = 2
+
+        if score == 500:
+            mainScreen.blit(graphics.rave.cutscene6, (0, 0))
+            if keys[pygame.K_RETURN]:
+                rave = False
+                menu = True
+            if keys[pygame.K_r]:
+                score = 0
 
     pygame.display.update()
